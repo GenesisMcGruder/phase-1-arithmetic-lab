13675fa4ce88953334082e545427975bed1ff0e3
Genesis McGruder <genesismcgruder@gmail.com>
2023-04-30 13:36:51 -0400

    finished

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2;
+
+let random = Math.ceil(Math.random() * 10) +1;
+
+let num3 = 22
+let num4 = 6
+const mod = num3%num4;
+
+let max= Math.max(15,16,17,18,19,20);
+
